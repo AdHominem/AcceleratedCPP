f565eb508b6909b23ef4df6f0b026b81bcc46703
jk <vargsmane@gmx.de>
2017-07-09 00:36:31 +0200

    Added 3.3

--- 3.cpp
+++ 3.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <math.h>
+#include <algorithm>
 
 using namespace std;
 
@@ -13,10 +14,35 @@ void quartiles(vector<int> integers) {
     const vector<int>::iterator middle_index = integers.begin() + integers.size() / 2;
 
     const vector<int> first(integers.begin(), middle_index);
-    cout << "First quartil: " << median(first) << endl;
+    cout << "First quartile: " << median(first) << endl;
 
     const vector<int> second(integers.size() % 2 == 0 ? middle_index
                                                       : middle_index + 1, integers.end());
-    cout << "Third quartil: " << median(second);
+    cout << "Third quartile: " << median(second);
+}
 
+vector<string> filter(vector<string> v, string key) {
+    vector<string> result;
+    for (string word : v) {
+        if (word.compare(key) != 0) {
+            result.push_back(word);
+        }
+    }
+    return result;
+}
+
+void count_words() {
+    cout << "Please enter some words: \n";
+
+    vector<string> words;
+    string word;
+    while (cin >> word && word.compare("end") != 0) {
+        words.push_back(word);
+    }
+
+    while (!words.empty()) {
+        word = words[0];
+        cout << word << ": " << count(words.begin(), words.end(), word) << endl;
+        words = filter(words, word);
+    }
 }
